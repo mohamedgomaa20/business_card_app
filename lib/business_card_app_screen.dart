e819bf37c5bca48717788b4b07f94982529faf86
mohamedgomaa20 <20monagy@gmail.com>
2025-10-17 05:06:27 +0300

    qr code done

--- lib/business_card_app_screen.dart
+++ lib/business_card_app_screen.dart
@@ -1,13 +1,194 @@
-import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
 
-class BusinessCardAppScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:share_plus/share_plus.dart';
+
+class BusinessCardAppScreen extends StatefulWidget {
   const BusinessCardAppScreen({super.key});
 
+  @override
+  State<BusinessCardAppScreen> createState() => _BusinessCardAppScreenState();
+}
+
+class _BusinessCardAppScreenState extends State<BusinessCardAppScreen> {
   @override
   Widget build(BuildContext context) {
     const Color primaryColor = Color(0xFF0F1419);
     const Color accentColor = Color(0xFF00D9FF);
     const Color secondaryAccent = Color(0xFF00B894);
+    void _showQRCodeDialog(Color accentColor) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return Dialog(
+            backgroundColor: primaryColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadiusGeometry.circular(20),
+
+              side: BorderSide(color: accentColor, width: 2),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "My QR Code",
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 1,
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+
+                  Container(
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      border: Border.all(
+                        color: accentColor.withOpacity(.3),
+                        width: 1.5,
+                      ),
+                      gradient: LinearGradient(
+                        colors: [
+                          Colors.white.withOpacity(.08),
+                          Colors.white.withOpacity(.04),
+                        ],
+                      ),
+                    ),
+                    child: QrImageView(
+                      size: 200,
+                      data:
+                          'https://github.com/mohamedgomaa20 | linkedin.com/in/mohamed-ali-ahmed | 20monagy@gmail.com | (+20) 1002418816',
+                      backgroundColor: const Color(0xFF0F1419),
+                      foregroundColor: accentColor,
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton.icon(
+                        onPressed: () async {
+                          try {
+                            showDialog(
+                              context: context,
+                              barrierDismissible: false,
+                              builder: (context) {
+                                return Dialog(
+                                  backgroundColor: primaryColor,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(20.0),
+                                    child: Column(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: [
+                                        CircularProgressIndicator(
+                                          valueColor: AlwaysStoppedAnimation(
+                                            accentColor,
+                                          ),
+                                        ),
+                                        SizedBox(height: 20),
+                                        Text(
+                                          "Preparing the share...",
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 18,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                );
+                              },
+                            );
+                            final qrPainter = QrPainter(
+                              data:
+                                  'https://github.com/mohamedgomaa20 | https://www.linkedin.com/in/mohamed-gomaa-874133284 | 20monagy@gmail.com | (+20) 1002418816',
+
+                              version: QrVersions.auto,
+                              gapless: true,
+                              color: accentColor,
+                              emptyColor: primaryColor,
+                            );
+
+                            final image = await qrPainter.toImage(500);
+                            final byteData = await image.toByteData(
+                              format: ui.ImageByteFormat.png,
+                            );
+
+                            if (mounted) Navigator.pop(context);
+
+                            if (byteData != null) {
+                              await Share.shareXFiles([
+                                XFile.fromData(
+                                  byteData.buffer.asUint8List(),
+                                  mimeType: "image/png",
+                                  name: "Mohamed_Gomaa_QR.png",
+                                ),
+                              ], text: "My QR Code - Business Card");
+                            }
+                          } catch (error) {
+                            print(error);
+                            if (mounted) {
+                              Navigator.pop(context);
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(content: Text("Error $error")),
+                              );
+                            }
+                          }
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: accentColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadiusGeometry.circular(12),
+                          ),
+                        ),
+
+                        icon: Icon(Icons.share, color: Colors.black),
+                        label: Text(
+                          "Share",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+
+                      ElevatedButton.icon(
+                        onPressed: () => Navigator.pop(context),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.transparent,
+                          side: BorderSide(width: 2, color: accentColor),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadiusGeometry.circular(12),
+                          ),
+                        ),
+
+                        icon: Icon(Icons.close, color: accentColor),
+
+                        label: Text(
+                          "Cancel",
+                          style: TextStyle(
+                            color: accentColor,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      );
+    }
 
     return Scaffold(
       backgroundColor: primaryColor,
@@ -165,50 +346,56 @@ class BusinessCardAppScreen extends StatelessWidget {
                       ),
                       const SizedBox(height: 32),
 
-                      Card(
-                        color: Colors.white,
-
-                        margin: const EdgeInsets.symmetric(
-                          vertical: 8,
-                          horizontal: 14,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: const ListTile(
-                          leading: Icon(
-                            Icons.call,
-                            size: 30,
-                            color: Color(0xFF2b455f),
-                          ),
-                          title: Text(
-                            "(+20) 1002418816",
-                            style: TextStyle(fontSize: 22),
-                          ),
-                        ),
+                      BuildInfoCard(
+                        icon: Icons.call,
+                        text: "(+20) 1002418816",
+                        accentColor: accentColor,
+                        primaryColor: primaryColor,
+                        onTap: () {},
                       ),
-
-                      Card(
-                        color: Colors.white,
-
-                        margin: const EdgeInsets.symmetric(
-                          vertical: 8,
-                          horizontal: 14,
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                        child: const ListTile(
-                          leading: Icon(
-                            Icons.mail,
-                            size: 30,
-                            color: Color(0xFF2b455f),
+                      BuildInfoCard(
+                        icon: Icons.mail_rounded,
+                        text: "20monagy@gmail.com",
+                        accentColor: accentColor,
+                        primaryColor: primaryColor,
+                        onTap: () {},
+                      ),
+                      BuildInfoCard(
+                        icon: Icons.code_rounded,
+                        text: "github.com/mohamedgomaa20",
+                        accentColor: accentColor,
+                        primaryColor: primaryColor,
+                        onTap: () {},
+                      ),
+                      BuildInfoCard(
+                        icon: Icons.business_rounded,
+                        text:
+                            "https://www.linkedin.com/in/mohamed-gomaa-874133284",
+                        accentColor: accentColor,
+                        primaryColor: primaryColor,
+                        onTap: () {},
+                      ),
+                      const SizedBox(height: 36),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          CustomButton(
+                            icon: Icons.qr_code_2_outlined,
+                            text: "QR Code",
+                            accentColor: accentColor,
+                            isPrimary: true,
+                            onTap: () => _showQRCodeDialog(accentColor),
                           ),
-                          title: Text(
-                            "20monagy@gmail.com",
-                            style: TextStyle(fontSize: 22),
+                          const SizedBox(width: 16),
+
+                          CustomButton(
+                            icon: Icons.download_rounded,
+                            text: "DownLoad",
+                            accentColor: accentColor,
+                            isPrimary: false,
+                            onTap: () {},
                           ),
-                        ),
+                        ],
                       ),
                     ],
                   ),
@@ -217,6 +404,133 @@ class BusinessCardAppScreen extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    super.key,
+    required this.icon,
+    required this.text,
+    required this.accentColor,
+    required this.isPrimary,
+    required this.onTap,
+  });
+
+  final IconData icon;
+  final String text;
+  final Color accentColor;
+  final bool isPrimary;
+
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          border: !isPrimary
+              ? Border.all(color: accentColor.withOpacity(.5), width: 2)
+              : null,
+
+          gradient: isPrimary
+              ? LinearGradient(
+                  colors: [accentColor, accentColor.withOpacity(.8)],
+                )
+              : null,
+          boxShadow: isPrimary
+              ? [
+                  BoxShadow(
+                    color: accentColor.withOpacity(.4),
+                    blurRadius: 20,
+                    offset: Offset(0, 8),
+                  ),
+                ]
+              : [],
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 28, vertical: 14),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(icon, color: isPrimary ? Colors.white : accentColor, size: 22),
+            SizedBox(width: 10),
+            Text(
+              text,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w700,
+
+                color: isPrimary ? Colors.white : Colors.cyanAccent,
+                letterSpacing: .8,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BuildInfoCard extends StatelessWidget {
+  const BuildInfoCard({
+    super.key,
+    required this.icon,
+    required this.text,
+    required this.accentColor,
+    required this.primaryColor,
+    required this.onTap,
+  });
+
+  final IconData icon;
+  final String text;
+  final Color accentColor;
+  final Color primaryColor;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(color: Colors.white.withOpacity(0.1), width: 1.5),
+          gradient: LinearGradient(
+            colors: [
+              accentColor.withOpacity(.05),
+              accentColor.withOpacity(0.02),
+            ],
+          ),
+        ),
+        child: ListTile(
+          contentPadding: EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+          leading: Container(
+            padding: EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              gradient: LinearGradient(
+                colors: [
+                  accentColor.withOpacity(.2),
+                  accentColor.withOpacity(.1),
+                ],
+              ),
+            ),
+            child: Icon(icon, color: accentColor, size: 28),
+          ),
+          title: Text(
+            text,
+            style: TextStyle(
+              fontSize: 17,
+              fontWeight: FontWeight.w500,
+              color: Colors.white,
+            ),
+          ),
+        ),
       ),
     );
   }
